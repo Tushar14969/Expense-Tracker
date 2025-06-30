44a6cfd591165d379676a30ed8ab2a361a64dea6
Tushar14969 <130268128+Tushar14969@users.noreply.github.com>
2025-06-30 22:25:11 +0530

    added the button and current date

--- ExpenseTrackerWeb/src/app/components/expense-list/expense-list.component.ts
+++ ExpenseTrackerWeb/src/app/components/expense-list/expense-list.component.ts
@@ -1,12 +1,13 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-expense-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './expense-list.component.html',
   styleUrl: './expense-list.component.css'
 })
 export class ExpenseListComponent {
-
+  now:Date = new Date();
 }
