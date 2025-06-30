9372cd0faa74917fa4e2165f0e10934e1285d2e5
Tushar14969 <130268128+Tushar14969@users.noreply.github.com>
2025-06-30 21:31:18 +0530

    designed the landing page

--- ExpenseTrackerWeb/src/app/app.component.ts
+++ ExpenseTrackerWeb/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ExpenseListComponent } from './components/expense-list/expense-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ExpenseListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
