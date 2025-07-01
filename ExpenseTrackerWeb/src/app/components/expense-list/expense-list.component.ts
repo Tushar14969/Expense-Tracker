36b507e1fef983781c90e78caa738cc9c38fc82f
Tushar14969 <130268128+Tushar14969@users.noreply.github.com>
2025-07-01 21:46:57 +0530

    tried to display the items

--- ExpenseTrackerWeb/src/app/components/expense-list/expense-list.component.ts
+++ ExpenseTrackerWeb/src/app/components/expense-list/expense-list.component.ts
@@ -1,13 +1,15 @@
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-expense-list',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, CommonModule],
   templateUrl: './expense-list.component.html',
   styleUrl: './expense-list.component.css'
 })
 export class ExpenseListComponent {
   now:Date = new Date();
+
+  expenses:string[] = ['item 1','item 2','item 3'];
 }
